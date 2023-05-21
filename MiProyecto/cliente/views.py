2b83e7933cfcf5da9e6d08c1247245b485a2e19a
Guido Rosales <guidorosales22@gmail.com>
2023-05-21 19:24:06 -0300

    actualizacion models

--- MiProyecto/cliente/views.py
+++ MiProyecto/cliente/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Cliente
 
 
 def index(request):
-    return render(request, "cliente/index_cliente.html")
+    clientes = Cliente.objects.all()
+    datos = {"clientes" : clientes}
+    return render(request, "cliente/index_cliente.html", datos)
